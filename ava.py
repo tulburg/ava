22e9506308ec959514696a120807f583a2205b6d
tulburg <tulburg2@gmail.com>
2019-05-19 16:55:37 +0200

    switch mod_spacy to testing

--- ava.py
+++ ava.py
@@ -3,8 +3,8 @@ from lib import Mod_Spacy
 spacy = Mod_Spacy()
 
 def main() :
-  spacy.train()
-  # spacy.tokenize("The game is not loading");
+  # spacy.train()
+  spacy.tokenize("The game is not loading");
   # spacy.update_model("my car is red in color", [{"my":"(user)", "is":"-"}])
 
 if __name__ == '__main__':
